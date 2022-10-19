9c4fd22de84da3b1a0d31fab265e22a7d1f3d8dc
johnsci911 <johnguitarizta@gamil.com>
2022-10-20 02:27:49 +0800

    Using resolvers

---
+++ app/GraphQL/Mutations/CreatePostResolver.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\Post;
+
+final class CreatePostResolver
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+		return Post::create([
+			'user_id' => $args['user_id'],
+			'title' => $args['title'],
+			'body' => $args['body'],
+		]);
+    }
+}
