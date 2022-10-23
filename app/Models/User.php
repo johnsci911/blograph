ae863972e408a8c01e1e8dc663ce1c262658deac
johnsci911 <johnguitarizta@gamil.com>
2022-10-24 03:58:35 +0800

    Auth in lighthouse

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\Contracts\HasApiTokens as HasAPITokensContract;
 
-class User extends Authenticatable
+class User extends Authenticatable implements HasAPITokensContract
 {
     use HasApiTokens, HasFactory, Notifiable;
 
